6dbdcd48dc4892ea4517248a36c6c3446bc87067
Joshua Evans <thejoshuaevans@gmail.com>
2025-04-07 13:21:21 -0700

    Lab 2 pt 2 - "Class-ified" the concentration game - Added `wait` utility that wait for a certain amount of time. A classic I wind up putting in pretty much all my projects :D

--- docs/labs/lab2/concentration/src/index.js
+++ docs/labs/lab2/concentration/src/index.js
@@ -1,253 +1,345 @@
 import shuffleArray from '../../../../src/utils/shuffle-array.js';
+import wait from '../../../../src/utils/wait.js';
+
+/**
+ * @typedef Picks
+ * @prop {Card} first The first card that was picked
+ * @prop {Card} second The second card that was picked
+ */
+
+/**
+ * @typedef Status
+ * @prop {number} matches The number of matches found
+ * @prop {number} tries The number of tries taken
+ */
 
 //* Constants
+/** Base path of the folder where images can be found */
+const IMAGE_PATH_BASE = 'cards/';
+
 /** Path to the image of a card back */
-const CARD_BACK_IMG_PATH = 'cards/black_back.jpg';
+const CARD_BACK_IMG_PATH = `${IMAGE_PATH_BASE}black_back.jpg`;
+
 /** How long to wait before resetting a round */
 const ROUND_WAIT_TIME_MS = 1000;
 
-//* Global variables
-/** The folder where your card images are stored */
-const imagePath = 'cards/';
-
-/** An array that stores the images for each card */
-let images = Array(20).fill(null);
-
-/** The index of the first card picked by the user */
-let firstPick = -1;
-
-/** The index of the second card picked by the user */
-let secondPick = -1;
-
-/** The current number of discovered matches */
-let matches = 0;
-
-/** The current number of match attempts */
-let tries = 0;
-
 /**
- * Initializes the page
+ * A single card on the game board
  */
-function init()
-{
-  // Do a bunch of initialization tasks
-  fillImages();
-  shuffleImages();
-  showMatches();
-  enableAllCards();
-  showAllBacks();
-}
+class Card {
+  /**
+   * The board that this card is on
+   * @type {Board}
+   */
+  board = null;
 
-/**
- * Display the current number of matches and tries on the page
- */
-function showMatches() {
-  const statusElem = document.getElementById('status');
-  statusElem.innerHTML = `Matches: ${matches} Tries: ${tries}`;
-}
+  /**
+   * The HTML element that represents the card
+   * @type {HTMLElement}
+   */
+  element = null;
 
-/**
- * Fills the images array with 10 pre-set pairs of card filenames. Card filenames follow this pattern:
- * "card{v}{s}.jpg" where v is the first char of the value of the card and s is the first char of the
- * suit of the card. Example: "cardjh.jpg" is the jack of hearts
- */
-function fillImages() {
-  const values = ['a', 'k', 'q', 'j', 't', '9', '8', '7', '6', '5'];
-  const suits = ['h', 's'];
+  /**
+   * If this card is enabled
+   */
+  enabled = false;
 
-  let index = 0;
-  values.forEach((value) => {
-    suits.forEach((suit) => {
-      images[index] = `${imagePath}card${value}${suit}.jpg`;
-      index++;
-    });
-  });
-}
+  /**
+   * The ID of this card
+   */
+  id = -1;
 
-/**
- * Shuffle the elements in the images array
- */
-function shuffleImages() {
-  images = shuffleArray(images);
-}
+  /**
+   * The filename of this card's front image
+   */
+  imageName = '';
 
-/**
- * "Enable" the provided cards by assigning the handleClick function to the onclick event for all cards
- * and sets the cursor type to 'pointer'
- *
- * @param {HTMLElement[]} cards An array of card elements to enable
- */
-function enableCards(cards) {
-  cards.forEach((card) => {
-    card.onclick = handleClick;
-    card.classList.add('pointer');
-  });
-}
+  /**
+   * If this card has been removed from the board
+   */
+  removed = false;
 
-/**
- * Enable all the cards
- */
-function enableAllCards() {
-  const cards = Array.from(document.getElementsByName('card'));
+  /**
+   * Enable this card
+   */
+  enable = () => {
+    this.enabled = true;
+    this.element.onclick = this.handleCardClick;
+    this.element.classList.add('pointer');
+  };
 
-  enableCards(cards);
-}
+  /**
+   * Disable this card
+   */
+  disable = () => {
+    this.enabled = false;
+    this.element.onclick = null;
+    this.element.classList.remove('pointer');
+  };
 
-/**
- * Enable only the cards that have a background image set to anything other than "none"
- */
-function enableAllRemainingCards() {
-  const remainingCards = Array.from(document.getElementsByName('card')).filter((card) => {
-    return card.style.backgroundImage !== 'none';
-  });
+  /**
+   * Show the back of the card
+   */
+  showBack = () => {
+    this.element.style.backgroundImage = `url(${CARD_BACK_IMG_PATH})`;
+  };
 
-  enableCards(remainingCards);
-}
+  /**
+   * Show the front of the card
+   */
+  showFront = () => {
+    this.element.style.backgroundImage = `url(${this.imageName})`;
+  };
 
-/**
- * Disable the provided single card by doing the inverse of "enableCards"
- *
- * @param {HTMLElement} card An array of card elements to enable
- */
-function disableCard(card) {
-  card.onclick = null;
-  card.classList.remove('pointer');
-}
+  /**
+   * Remove the card from the board
+   */
+  removeCard = () => {
+    this.element.style.backgroundImage = 'none';
+    this.removed = true;
+  };
 
-/**
- * Disable the provided cards by doing the inverse of "enableCards"
- *
- * @param {HTMLElement[]} cards An array of card elements to enable
- */
-function disableCards(cards) {
-  cards.forEach((card) => {
-    disableCard(card);
-  });
-}
+  /**
+   * Handler for when the user clicks on a card
+   *
+   * @this {Card} The card that was clicked
+   */
+  handleCardClick = () => {
+    this.disable();
+    this.showFront();
 
-/**
- * Disable all the cards
- */
-function disableAllCards() {
-  const cards = Array.from(document.getElementsByName('card'));
-  disableCards(cards);
-}
+    const picks = this.board.game.picks;
 
-/**
- * Completely "remove" a card from the board by setting the background image to "none"
- *
- * @param {HTMLElement} card The card to remove from the board
- */
-function removeCard(card) {
-  card.style.backgroundImage = 'none';
-}
-
-/**
- * Remove a card from the board by its index
- *
- * @param {number} index The index of the card to retrieve
- */
-function removeCardByIndex(index) {
-  const card = document.getElementById(index);
-  removeCard(card);
-}
-
-/**
- * Show the back of a particular card by setting the background image accordingly
- *
- * @param {HTMLElement} card The card to show
- */
-function showBack(card) {
-  card.style.backgroundImage = `url(${CARD_BACK_IMG_PATH})`;
-}
-
-/**
- * Shows the back of a card by setting the background image to the "back-of-card" image
- *
- * @param {number} index The index of the card to show the back of
- */
-function showBackByIndex(index) {
-  const card = document.getElementById(index);
-  card.style.backgroundImage = `url(${CARD_BACK_IMG_PATH})`;
-}
-
-/**
- * Iterate through all cards and shows the backs of each one
- */
-function showAllBacks() {
-  const cards = Array.from(document.getElementsByName('card'));
-  cards.forEach((card) => {
-    showBack(card);
-  });
-}
-
-/**
- * Function that fires when the user clicks on a card. Will perform game logic depending on if this is
- * the first or second card clicked
- */
-function handleClick() {
-  const index = this.id;
-  const cardImage = images[index];
-
-  this.style.backgroundImage = `url(${cardImage})`;
-  disableCard(this);
-
-  if (firstPick === -1) {
-    firstPick = index;
-  } else {
-    secondPick = index;
-    disableAllCards();
-    setTimeout(checkCards, ROUND_WAIT_TIME_MS);
-  }
-}
-
-/**
- * Determines if the cards indicated by the firstPick and secondPick variables are a match
- *
- * @returns {boolean} True if the two cards are a match, false otherwise
- */
-function isMatch() {
-  const firstCard = images[firstPick];
-  const secondCard = images[secondPick];
-
-  const cardValueRegex = /\/card(.).\.jpg$/;
-  const firstCardValue = cardValueRegex.exec(firstCard)[1];
-  const secondCardValue = cardValueRegex.exec(secondCard)[1];
-
-  return firstCardValue === secondCardValue;
-}
-
-/**
- * Check the two cards that have been picked for matches and act accordingly. Resets the turn and triggers
- * victory if the required number of matches has been reached
- */
-function checkCards() {
-  tries++;
-
-  if (isMatch()) {
-    // This is a match! Increment the match counter and remove the cards from the board
-    matches++;
-    removeCardByIndex(firstPick);
-    removeCardByIndex(secondPick);
-
-    // Check for win condition
-    if (matches < 10) {
-      enableAllRemainingCards();
+    if (picks.first === null) {
+      picks.first = this;
     } else {
-      alert('You win!');
+      picks.second = this;
+      this.board.game.checkPicks();
     }
-  } else {
-    // Not a match! Turn the cards back over
-    showBackByIndex(firstPick);
-    showBackByIndex(secondPick);
-    enableAllRemainingCards();
-  }
+  };
 
-  // Show the current game state and reset the picks for the next turn
-  showMatches();
-  firstPick = -1;
-  secondPick = -1;
+  /**
+   * @param {Card} props Properties to apply to the card
+   */
+  constructor(props) {
+    this.board = props.board;
+    this.id = props.id;
+    this.imageName = props.imageName;
+
+    this.element = document.getElementById(this.id);
+  }
 }
 
-// when the page loads, call the function init
-window.onload = init;
+/**
+ * The game board upon which cards are placed
+ */
+class Board {
+  /**
+   * The game that is managing this board
+   * @type {Game}
+   */
+  game = null;
+
+  /**
+   * All the cards on the board
+   * @type {Card[]}
+   */
+  cards = [];
+
+  /**
+   * Enable all the cards on the board
+   */
+  enableAllCards = () => {
+    this.cards.forEach((card) => {
+      card.enable();
+    });
+  };
+
+  /**
+   * Disable all the cards on the board
+   */
+  disableAllCards = () => {
+    this.cards.forEach((card) => {
+      card.disable();
+    });
+  };
+
+  /**
+   * Show all the card backs on the board
+   */
+  showAllCardBacks = () => {
+    this.cards.forEach((card) => {
+      card.showBack();
+    });
+  };
+
+  /**
+   * Enable all the cards that have not yet been removed from the board
+   */
+  enableAllRemainingCards = () => {
+    this.cards.forEach((card) => {
+      if (!card.removed) {
+        card.enable();
+      }
+    });
+  };
+
+  /**
+   * Generate a return a shuffled array of card image names
+   */
+  generateShuffledImageNames = () => {
+    const values = ['a', 'k', 'q', 'j', 't', '9', '8', '7', '6', '5'];
+    const suits = ['h', 's'];
+    const images = [];
+
+    let index = 0;
+    values.forEach((value) => {
+      suits.forEach((suit) => {
+        images[index] = `${IMAGE_PATH_BASE}card${value}${suit}.jpg`;
+        index++;
+      });
+    });
+
+    return shuffleArray(images);
+  };
+
+  /**
+   * Initialize the cards on the board
+   */
+  initializeCards = () => {
+    const shuffledImageNames = this.generateShuffledImageNames();
+    shuffledImageNames.forEach((imageName, id) => {
+      const card = new Card({imageName, id, board: this});
+      this.cards.push(card);
+    });
+  };
+
+  /**
+   * Initialize the board
+   */
+  init = () => {
+    this.initializeCards();
+    this.enableAllCards();
+    this.showAllCardBacks();
+  };
+
+  /**
+   * @param {Board} props
+   */
+  constructor(props) {
+    this.game = props.game;
+  }
+}
+
+/**
+ * Game manager
+ */
+class Game {
+  /**
+   * The board of cards
+   * @type {Board}
+   */
+  board = null;
+
+  /**
+   * The current game status
+   * @type {Status}
+   */
+  status = {
+    matches: 0,
+    tries: 0,
+  };
+
+  /**
+   * The HTML element that shows the status of the game
+   * @type {HTMLElement}
+   */
+  statusElement = null;
+
+  /**
+   * @type {Picks}
+   */
+  picks = {
+    first: null,
+    second: null,
+  };
+
+  /**
+   * Check if two picked cards are a match. Returns true if they are a match, false otherwise
+   *
+   * @param {Picks} picks The cards that were picked
+   */
+  isMatch(picks) {
+    const {first: firstCard, second: secondCard} = picks;
+
+    const cardValueRegex = /\/card(.).\.jpg$/;
+    const firstCardValue = cardValueRegex.exec(firstCard.imageName)[1];
+    const secondCardValue = cardValueRegex.exec(secondCard.imageName)[1];
+
+    return firstCardValue === secondCardValue;
+  }
+
+  /**
+   * Check the current picks to see if they are a match, then adjust the game state accordingly
+   */
+  checkPicks = async () => {
+    this.board.disableAllCards();
+
+    // Wait a little bit so the user can see the revealed cards
+    await wait(ROUND_WAIT_TIME_MS);
+
+    this.status.tries++;
+    const isMatch = this.isMatch(this.picks);
+    if (isMatch) {
+      // It's a match!
+      this.status.matches++;
+      this.picks.first.removeCard();
+      this.picks.second.removeCard();
+
+      // Check win condition
+      if (this.status.matches >= 10) {
+        alert('You win!');
+      } else {
+        this.board.enableAllRemainingCards();
+      }
+
+    } else {
+      // It's not a match, reset the turn :(
+      this.picks.first.showBack();
+      this.picks.second.showBack();
+      this.board.enableAllRemainingCards();
+    }
+
+    // Show the current game state and reset the picks for the next turn
+    this.showMatches();
+    this.picks.first = null;
+    this.picks.second = null;
+  };
+
+  /**
+   * Display the current number of matches and tries on the page in the status element
+   */
+  showMatches = () => {
+    this.statusElement.innerHTML = `Matches: ${this.status.matches} Tries: ${this.status.tries}`;
+  };
+
+  /**
+   * Run the game!
+   */
+  run = () => {
+    this.board = new Board({game: this});
+    this.board.init();
+  };
+
+  constructor() {
+    this.statusElement = document.getElementById('status');
+  }
+}
+
+// Create and run the game on load
+window.onload = async () => {
+  const game = new Game();
+  window.game = game;
+
+  game.run();
+};
